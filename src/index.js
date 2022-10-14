5aefc42aa469ab4e7fc8a37fc17d357d7b9e32eb
Valentin Mari <valentinmari@hotmail.com>
2022-10-14 13:29:46 +0000

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -1,58 +1,62 @@
-const {createBullBoard} = require('@bull-board/api');
-const {BullAdapter} = require('@bull-board/api/bullAdapter');
-const {BullMQAdapter} = require('@bull-board/api//bullMQAdapter');
-const {ExpressAdapter} = require('@bull-board/express');
-const Queue = require('bull');
-const bullmq = require('bullmq');
-const express = require('express');
-const redis = require('redis');
-const session = require('express-session');
-const passport = require('passport');
-const {ensureLoggedIn} = require('connect-ensure-login');
-const bodyParser = require('body-parser');
+const { createBullBoard } = require("@bull-board/api");
+const { BullAdapter } = require("@bull-board/api/bullAdapter");
+const { BullMQAdapter } = require("@bull-board/api//bullMQAdapter");
+const { ExpressAdapter } = require("@bull-board/express");
+const Queue = require("bull");
+const bullmq = require("bullmq");
+const express = require("express");
+const redis = require("redis");
+const session = require("express-session");
+const passport = require("passport");
+const { ensureLoggedIn } = require("connect-ensure-login");
+const bodyParser = require("body-parser");
 
-const {authRouter} = require('./login');
-const config = require('./config');
+const { authRouter } = require("./login");
+const config = require("./config");
 
 const redisConfig = {
 	redis: {
 		port: config.REDIS_PORT,
 		host: config.REDIS_HOST,
 		db: config.REDIS_DB,
-		...(config.REDIS_PASSWORD && {password: config.REDIS_PASSWORD}),
-		tls: config.REDIS_USE_TLS === 'true',
+		...(config.REDIS_PASSWORD && { password: config.REDIS_PASSWORD }),
+		tls: config.REDIS_USE_TLS === "true",
 	},
 };
 
 const serverAdapter = new ExpressAdapter();
 const client = redis.createClient(redisConfig.redis);
-const {setQueues} = createBullBoard({queues: [], serverAdapter});
+const { setQueues } = createBullBoard({ queues: [], serverAdapter });
 const router = serverAdapter.getRouter();
 
 client.KEYS(`${config.BULL_PREFIX}:*`, (err, keys) => {
-	const uniqKeys = new Set(keys.map(key => key.replace(/^.+?:(.+?):.+?$/, '$1')));
-	const queueList = Array.from(uniqKeys).sort().map(
-		(item) => {
-			if (config.BULL_VERSION === 'BULLMQ') {
-				return new BullMQAdapter(new bullmq.Queue(item, {connection: redisConfig.redis}));
+	const uniqKeys = new Set(
+		keys.map((key) => key.replace(/^.+?:(.+?):.+?$/, "$1"))
+	);
+	const queueList = Array.from(uniqKeys)
+		.sort()
+		.map((item) => {
+			if (config.BULL_VERSION === "BULLMQ") {
+				return new BullMQAdapter(
+					new bullmq.Queue(item, { connection: redisConfig.redis })
+				);
 			}
 
 			return new BullAdapter(new Queue(item, redisConfig));
-		}
-	);
+		});
 
 	setQueues(queueList);
-	console.log('done!')
+	console.log("done!");
 });
 
 const app = express();
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
-if (app.get('env') !== 'production') {
-	const morgan = require('morgan');
-	app.use(morgan('combined'));
+if (app.get("env") !== "production") {
+	const morgan = require("morgan");
+	app.use(morgan("combined"));
 }
 
 app.use((req, res, next) => {
@@ -64,21 +68,21 @@ app.use((req, res, next) => {
 });
 
 const sessionOpts = {
-	name: 'bull-board.sid',
+	name: "bull-board.sid",
 	secret: Math.random().toString(),
 	resave: false,
 	saveUninitialized: false,
 	cookie: {
-		path: '/',
+		path: "/",
 		httpOnly: false,
-		secure: false
-	}
+		secure: false,
+	},
 };
 
 app.use(session(sessionOpts));
 app.use(passport.initialize({}));
 app.use(passport.session({}));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 if (config.AUTH_ENABLED) {
 	app.use(config.LOGIN_PAGE, authRouter);
@@ -88,6 +92,9 @@ if (config.AUTH_ENABLED) {
 }
 
 app.listen(config.PORT, () => {
-	console.log(`bull-board is started http://localhost:${config.PORT}${config.HOME_PAGE}`);
+	console.log(
+		`bull-board is started http://localhost:${config.PORT}${config.HOME_PAGE}`
+	);
 	console.log(`bull-board is fetching queue list, please wait...`);
 });
+
